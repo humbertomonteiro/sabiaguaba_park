0201217f79545f918711ee371ba08d57d39b38c0
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-05-26 11:31:15 -0300

    att

--- js/home.js
+++ js/home.js
@@ -28,3 +28,49 @@ toggle.onclick = e => {
     navLinks.classList.toggle('show-links')
     navLinks.classList.toggle('hidden-links')
 }
+
+
+
+window.sr = ScrollReveal({ reset: true })
+
+
+
+sr.reveal('.header', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.slider', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.container-text', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.container-aboutUs', {
+    rotate: { x: 0, y: 80, z:0 }, 
+    duration: 2000 
+})
+
+sr.reveal('.menu', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.container-promotion', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.container-location', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
+
+sr.reveal('.experience', { 
+    rotate: { x: 0, y: 80, z:0 },
+    duration: 2000 
+})
