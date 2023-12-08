936793c49e3c8cad18b331707a3e461b258850db
Humberto Monteiro <humbertoaraujox@icloud.com>
2023-12-08 14:11:42 -0300

    att

--- js/audio.js
+++ js/audio.js
@@ -1,50 +1,59 @@
 const audios = [
-  { audio: "./audio/Coldplay-Paradise.mp3", duration: "252000" },
-  { audio: "./audio/JasonMraz-I'mYours.mp3", duration: "204600" },
-  { audio: "./audio/MariaGadú-Shimbalaiê.mp3", duration: "190200" },
-  { audio: "./audio/Natiruts-Ela.mp3", duration: "211800" },
-  { audio: "./audio/Natiruts-QueroSerFelizTambém.mp3", duration: "199800" },
-  { audio: "./audio/PharrellWilliams-Happy.mp3", duration: "240000" },
+  // { audio: "./audio/Coldplay-Paradise.mp3", duration: "252000" },
+  // { audio: "./audio/JasonMraz-I'mYours.mp3", duration: "204600" },
+  // { audio: "./audio/MariaGadú-Shimbalaiê.mp3", duration: "190200" },
+  // { audio: "./audio/Natiruts-Ela.mp3", duration: "211800" },
+  // { audio: "./audio/Natiruts-QueroSerFelizTambém.mp3", duration: "199800" },
+  // { audio: "./audio/PharrellWilliams-Happy.mp3", duration: "240000" },
+  { audio: "./audio/SIMPLESASSIM-PedroPondé.mp3", duration: "240000" },
 ];
 
 const divAudio = document.querySelector(".div-audio");
 const audio = document.querySelector("[audio]");
-const iconAudio = document.querySelector(".icon-audio");
+const divIcon = document.querySelector(".div-icon");
 
-let playAudio;
-
-playAudio = Math.floor(Math.random() * audios.length);
-
-audio.src = audios[playAudio].audio;
-
-let audioPause = false;
+let showControls = true;
 
 divAudio.onclick = () => {
-  audioPause = !audioPause;
+  showControls = !showControls;
 
-  if (!audioPause) {
-    audio.play();
-    divAudio.innerHTML = '<i class="fa-solid fa-volume-high icon-audio"></i>';
+  if (showControls) {
+    divIcon.innerHTML = '<i class="fa-solid fa-volume-high icon-audio"></i>';
+
+    audio.controls = false;
   } else {
-    audio.pause();
-    divAudio.innerHTML = '<i class="fa-solid fa-volume-xmark icon-audio"></i>';
+    divIcon.innerHTML = '<i class="fa-solid fa-volume-xmark icon-audio"></i>';
+
+    audio.controls = true;
+
+    setTimeout(() => {
+      audio.controls = false;
+      showControls = false;
+      divIcon.innerHTML = '<i class="fa-solid fa-volume-high icon-audio"></i>';
+    }, 6000);
   }
 };
 
-if (audioPause) {
-  setInterval(() => {
-    playAudio = Math.floor(Math.random() * audios.length);
-    audio.src = audios[playAudio].audio;
-  }, audios[playAudio].duration);
-} else {
-  audio.pause();
-}
+// let playAudio;
 
-const nextAudio = document.querySelector(".div-next");
+// playAudio = Math.floor(Math.random() * audios.length);
 
-nextAudio.onclick = () => {
-  playAudio = Math.floor(Math.random() * audios.length);
-  audio.src = audios[playAudio].audio;
-  divAudio.innerHTML = '<i class="fa-solid fa-volume-high icon-audio"></i>';
-  audioPause = true;
-};
+// audio.src = audios[0].audio;
+
+// if (audioPause) {
+//   setInterval(() => {
+//     playAudio = Math.floor(Math.random() * audios.length);
+//     audio.src = audios[playAudio].audio;
+//   }, audios[playAudio].duration);
+// } else {
+//   audio.pause();
+// }
+
+// const nextAudio = document.querySelector(".div-next");
+
+// nextAudio.onclick = () => {
+//   playAudio = Math.floor(Math.random() * audios.length);
+//   audio.src = audios[playAudio].audio;
+//   divAudio.innerHTML = '<i class="fa-solid fa-volume-high icon-audio"></i>';
+//   audioPause = true;
+// };
